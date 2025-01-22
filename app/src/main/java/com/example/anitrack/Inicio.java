162b956ae4666ebe80d05153f93690e2adbaa44c
SHZTD <tdrag@elpuig.xeill.net>
2025-01-22 17:55:10 +0100

    pantalla 1 "medio hecha"

--- app/src/main/java/com/example/anitrack/Inicio.java
+++ app/src/main/java/com/example/anitrack/Inicio.java
@@ -12,10 +12,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Inicio extends Fragment {
     Button btn1;
     NavController navController;
+
+    TextView texto1;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -27,8 +30,8 @@ public class Inicio extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         navController = Navigation.findNavController(view);
-
         btn1 = view.findViewById(R.id.btn1);
+        texto1 = view.findViewById(R.id.textView);
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,8 +39,17 @@ public class Inicio extends Fragment {
                 navController.navigate(R.id.tab1);
             }
         });
+
+        texto1.setText(
+                "Escucha, guarda o crea playlists\n" +
+                        "de OSTs, openings e incluso\n" +
+                        "bandas sonoras de animes\n" +
+                        "o videojuegos.\n" +
+                        "\n" +
+                        "Empieza a explorar en Anitrack."
+        );
+
+        // centra el texto
+        texto1.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
     }
 }
-
-
-
