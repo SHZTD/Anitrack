0700a7f60a13e48883a0425cd95b48e35cd50213
SHZTD <tdrag@elpuig.xeill.net>
2025-01-29 17:19:16 +0100

    inicio con tabbed activity y la pantalla Master

---
+++ app/src/main/java/com/example/anitrack/master.java
@@ -0,0 +1,47 @@
+package com.example.anitrack;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.example.anitrack.databinding.FragmentMasterBinding;
+
+
+public class master extends Fragment {
+    private FragmentMasterBinding binding;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return (binding = FragmentMasterBinding.inflate(inflater, container, false)).getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.viewPager.setAdapter(new FragmentStateAdapter(this) {
+            @NonNull
+            @Override
+            public Fragment createFragment(int position) {
+                switch (position) {
+                    case 0: default:
+                        return new Tab1();
+                    case 1:
+                        return new Tab2();
+                    case 2:
+                        return new Tab3();
+                }
+            }
+
+            @Override
+            public int getItemCount() {
+                return 3;
+            }
+        });
+    }
+}
